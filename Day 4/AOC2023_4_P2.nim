c994a30c3cac24d53aa664f51ac9aae42006ce3c
Alex . <Laaen@hotmail.fr>
2023-12-04 19:51:24 +0100

    Day 4 Arturo + refactored Nim

--- Day 4/AOC2023_4_P2.nim
+++ Day 4/AOC2023_4_P2.nim
@@ -1,22 +1,10 @@
 import std/strutils
-import std/sugar
+import std/math
+import std/sets
 
-# Slow code
-
-type
-    Card = object of RootObj
-        nb : int
-        content : seq[int]
-        winning : seq[int]
-
-proc newCard(c : seq[int], w : seq[int]) : Card =
-    Card(nb: 1, content: c, winning : w)
-
-proc winning(c :Card) : int =
+proc winning(c, w : openArray[int]) : int =
     ## Returns the number of winning n°
-    for elt in c.content:
-        if elt in c.winning:
-            result += 1
+    return toHashSet(c).intersection(toHashSet(w)).len
 
 proc strToIntArr(a : string) : seq[int] =
     for elt in a.split(" "):
@@ -28,17 +16,15 @@ iterator processFile(file_name : string): seq[seq[int]] =
         let infos = elt.split(":")[1].split("|")
         yield @[strToIntArr(infos[0].strip()), strToIntArr(infos[1].strip())]
 
-# Fill the cards list
-var cards_list = collect:
-    for elt in processFile("inputs"):
-        newCard(elt[1], elt[0])
 
-var total = 0
-# We iterate through each card X times (nb of card we have)
-for idx, card in cards_list:
-    for i in 0..card.nb - 1:
-        for w in 1..card.winning():
-            cards_list[idx + w].nb += 1
-    total += card.nb
+var cards : array[500, int]
+var i = 0
+# For each card
+for elt in processFile("inputs"):
+    # The original ticket
+    cards[i] += 1
+    for j in 1..winning(elt[0], elt[1]):
+        cards[j + i] += cards[i]
+    i += 1
 
-echo total
+echo sum(cards)
