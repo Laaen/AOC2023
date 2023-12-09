e86776f6293cf6824d1a62b326611e657c5e2a9c
Alex . <Laaen@hotmail.fr>
2023-12-09 19:15:08 +0100

    Day 9

---
+++ Day 9/AOC_9_P2.nim
@@ -0,0 +1,28 @@
+import sugar
+import strutils
+import sequtils
+import sets
+import strformat
+import algorithm
+
+proc processLine(a: seq[int], i : int) : int =
+    var res: seq[int]
+    # Process the line
+    for i in 0..(a.len - 2):
+        res.add(a[i + 1] - a[i])
+    if res.toHashSet.len != 1:
+        let delta = processLine(res, i + 1)
+        return a[^1] + delta
+    else:
+        return res[^1] + a[^1] 
+
+let infos = collect:
+    for l in lines("inputs"):
+        l.split(" ").map(x => x.parseInt)
+
+var res = 0
+for line in infos:
+    var current_line = line
+    res += processLine(current_line.reversed, 0)
+
+echo res
